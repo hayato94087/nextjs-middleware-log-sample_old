65a204a99f7c6d89f434fe8044cc4ef198cd08da
hayato94087 <hayato94087@gmail.com>
2023-08-15 11:58:45 +0900

    matcherを削除

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,3 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/"],
 });
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
